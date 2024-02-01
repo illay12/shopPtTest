5bc5d8e22620739d04dee9c2e96668eba7030242
laurikc <laurentiupetrica11@yahoo.com>
2024-02-01 15:57:27 +0200

    invat lazy loading

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -37,4 +37,9 @@ export class ShopService {
   {
     return this.http.get<Type[]>(this.baseUrl + 'products/types');
   }
+
+  getProduct(id: number)
+  {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
 }
